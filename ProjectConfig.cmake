9a785b16ad4fe5a399ce0d111b44b7d90cda32e8
LevPogromov <lew.cherezow@gmail.com>
2025-04-20 14:45:10 +0300

    #add class Receiver

--- ProjectConfig.cmake
+++ ProjectConfig.cmake
@@ -1,8 +1,8 @@
 # Путь до папки установки библиотеки C++
-set(CPP_LIB_INSTALL_DIR "cpplib/installation/path" CACHE PATH "Путь установки C++ библиотек")
+set(CPP_LIB_INSTALL_DIR "/usr/bin" CACHE PATH "Путь установки C++ библиотек")
 
 # Путь до Python интерпретатора
-set(PYTHON_EXECUTABLE "python/interpreter/path" CACHE nFILEPATH "Путь до Python интерпретатора")
+set(PYTHON_EXECUTABLE "/usr/bin/python" CACHE FILEPATH "Путь до Python интерпретатора")
 
 # Путь до папки установки библиотеки Python (можно в site-packages)
-set(PYTHON_LIB_INSTALL_DIR "python/package/installation/path" CACHE PATH "Путь установки Python библиотек")
+set(PYTHON_LIB_INSTALL_DIR "/usr/lib/python3.13/site-packages" CACHE PATH "Путь установки Python библиотек")
