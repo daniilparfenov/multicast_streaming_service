fa345df6532284f4d8277eb089c7586460f4a3a1
vadim <144333283+vadyukkha@users.noreply.github.com>
2025-04-21 23:14:22 +0300

    #fix config

--- ProjectConfig.cmake
+++
@@ -1,8 +0,0 @@
-# Путь до папки установки библиотеки C++
-set(CPP_LIB_INSTALL_DIR "cpplib/installation/path" CACHE PATH "Путь установки C++ библиотек")
-
-# Путь до Python интерпретатора
-set(PYTHON_EXECUTABLE "python/interpreter/path" CACHE FILEPATH "Путь до Python интерпретатора")
-
-# Путь до папки установки библиотеки Python (можно в site-packages)
-set(PYTHON_LIB_INSTALL_DIR "python/package/installation/path" CACHE PATH "Путь установки Python библиотек")
